4f0bd40a2b425dcf29124927f3a6486417a8fb27
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-05-01 15:55:42 -0400

    homescreen

--- screens/Game/index.js
+++ screens/Game/index.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 import { Header } from '../../components/Header';
 import styles from './styles';
 import PropTypes from 'prop-types';
+import { generateRGB, mutateRGB } from '../../utilities';
+
 
 export default class Home extends Component {
   state = {
